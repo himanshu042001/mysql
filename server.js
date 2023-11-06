10811d83fcdf2590632c020e257acabb230b4f90
himanshu <mhshekhar04@gmail.com>
2023-11-06 18:37:04 +0530

    now data will render on browser

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mysql = require('mysql');
-const cors = require('cors'); // Import the cors middleware
+const cors = require('cors');
+const path = require('path'); // Import the path module
 
 const app = express();
 const port = 5000;
@@ -35,7 +36,7 @@ app.use(express.static('public'));
 // Handle form submission
 app.post('/submit', (req, res) => {
     const { companyUEN, companyName, fullname, position, email, phone } = req.body;
-console.log(req)
+
     const sql = 'INSERT INTO CompanyData (CompanyUEN, CompanyName, FullName, Position, Email, Phone) VALUES (?, ?, ?, ?, ?, ?)';
 
     db.query(sql, [companyUEN, companyName, fullname, position, email, phone], (err, result) => {
@@ -44,7 +45,7 @@ console.log(req)
             res.status(500).send('Error inserting data into the database');
         } else {
             console.log('Data inserted into CompanyData table');
-            res.status(200).send({"message":"Data inserted into CompanyData table"});
+            res.status(200).send('Data inserted into the database');
         }
     });
 });
@@ -62,18 +63,11 @@ app.get('/fetchData', (req, res) => {
     });
 });
 
-// app.get('/', (req, res) => {
-//     res.sendFile(__dirname + '/public/result.html');
-// });
+// Modify this route to send the result.html page
+app.get('/result', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'result.html'));
+});
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
-});
-
-// Import the ResultPage component
-const ResultPage = require('./ResultPage');
-
-// Modify the route for the result page
-app.get('/result.html', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
